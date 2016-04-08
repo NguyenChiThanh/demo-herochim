549010bfe4cd733412b65b63fe8e62e571af5b47
Nguyễn Chí Thanh <51203336@hcmut.edu.vn>
2016-04-08 18:39:15 +0700

    use postgresql

--- connect-db.php
+++ connect-db.php
@@ -4,10 +4,10 @@
  Allows PHP to connect to your database
 */
 
-$username = "xkykkiturabxci";
-$password = "t_e7xImfxDqeM9D_W3KKB6Yr3T";
-$hostname = "ec2-54-235-85-65.compute-1.amazonaws.com";
-$dbname = "dcjfe6jau3gtmf"; 
+$username = "dflvrvuwmonmrm";
+$password = "g4Qv3ojRfVhovEhytps-SiioU7";
+$hostname = "ec2-23-21-249-224.compute-1.amazonaws.com";
+$dbname = "d4v2bpe8au03a9"; 
 
 // Create connection
 $conn = new pg_connect($hostname, $username, $password, $dbname);

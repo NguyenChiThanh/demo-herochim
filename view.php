cb83661f2b0331c3a267a1381215100c18dfa312
Nguyễn Chí Thanh <51203336@hcmut.edu.vn>
2016-04-08 22:25:42 +0700

    fix connect to db

--- view.php
+++ view.php
@@ -27,19 +27,14 @@ echo "<tr> <th>ID</th> <th>Name</th> <th>Year</th> <th></th> <th></th></tr>";
 
 	// loop through results of database query, displaying them in the table
 while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-
-		// echo out the contents of each row into a table
-	echo $row['id'];
-	echo $row['name'];
-	echo $row['year']
 	?>
-	<!-- <tr>
+	<tr>
 	<td class="id"><?php echo $row['id'] ?></td>
 	<td class="name"><?php echo $row['name'] ?></td>
 	<td class="year"><?php echo $row['year'] ?></td>
 	<td><input type="button" class="btn btn-primary edit" value="Edit"></td>
 	<td><input type="button" class="btn btn-danger delete" value="Delete"></td>
-	</tr>  -->
+	</tr> 
 	<?php
 } 
 	// close table

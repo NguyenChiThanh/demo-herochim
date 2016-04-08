9b138325129e6f36fd310468acf69b10d77bba30
Nguyễn Chí Thanh <51203336@hcmut.edu.vn>
2016-04-08 22:06:50 +0700

    fix connect to db

--- connect-db.php
+++ connect-db.php
@@ -4,31 +4,31 @@
  Allows PHP to connect to your database
 */
 
-// $username = "dflvrvuwmonmrm";
-// $password = "g4Qv3ojRfVhovEhytps-SiioU7";
-// $hostname = "ec2-23-21-249-224.compute-1.amazonaws.com";
-// $dbname = "d4v2bpe8au03a9"; 
+$username = "dflvrvuwmonmrm";
+$password = "g4Qv3ojRfVhovEhytps-SiioU7";
+$hostname = "ec2-23-21-249-224.compute-1.amazonaws.com";
+$dbname = "d4v2bpe8au03a9"; 
 
-// // Create connection
-// $conn = new pg_connect($hostname, $username, $password, $dbname);
-//   // Check connection
-// if ($conn->connect_error) {
-// 	die("Connection failed: " . $conn->connect_error);
+// Create connection
+$conn = new pg_connect($hostname, $username, $password, $dbname);
+  // Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+// function pg_connection_string() {
+//   return "dbname=d4v2bpe8au03a9 host=ec2-23-21-249-224.compute-1.amazonaws.com port=5432 user=dflvrvuwmonmrm password=g4Qv3ojRfVhovEhytps-SiioU7 sslmode=require";
 // }
-function pg_connection_string() {
-  return "dbname=d4v2bpe8au03a9 host=ec2-23-21-249-224.compute-1.amazonaws.com port=5432 user=dflvrvuwmonmrm password=g4Qv3ojRfVhovEhytps-SiioU7 sslmode=require";
-}
  
-# Establish db connection
-$conn = pg_connect(pg_connection_string());
-if (!$conn) {
-    echo "Database connection error."
-    exit;
-}
-else {
-	echo "Hello World";
-}
+// # Establish db connection
+// $conn = pg_connect(pg_connection_string());
+// if (!$conn) {
+//     echo "Database connection error."
+//     exit;
+// }
+// else {
+// 	echo "Hello World";
+// }
 $sql = "SELECT id, name, year FROM cars";
 $result = $conn->query($sql);
-echo $result; 
+echo "Success"; 
 ?>
